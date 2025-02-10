f347eb4bc73c5aad3add56ae3169463dbc73f322
Shadows-Hedgehog <shadowrouge45@gmail.com>
2025-02-10 17:20:51 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "file",
     version: "1.0",
     author: "OtinXShiva",
-    countDown: 5,
+    countDown: 5,l
     role: 0,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
@@ -14,9 +14,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61571952600731"];
+    const permission = ["61571952600731","100080355760429"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝐞𝐮𝐡 ? 😐. 𝐋𝐚𝐢𝐬𝐬𝐞 𝐜̧𝐚... 𝐭𝐮 𝐧'𝐞𝐬 𝐩𝐚𝐬 𝙴𝙶𝙶𝙼𝙰𝙽 𝚁𝙾𝙱𝙾𝚃𝙸𝚀𝚄𝙴 𝐦𝐨𝐧 𝐯𝐢𝐞𝐮𝐱 ", event.threadID, event.messageID);
+      return api.sendMessage("𝐞𝐮𝐡 ? 😐. 𝐋𝐚𝐢𝐬𝐬𝐞 ç𝐚... 𝐭𝐮 𝐧'𝐞𝐬 𝐩𝐚𝐬 𝙴𝙶𝙶𝙼𝙰𝙽 𝚁𝙾𝙱𝙾𝚃𝙸𝚀𝚄𝙴 𝐦𝐨𝐧 𝐯𝐢𝐞𝐮𝐱 ", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
